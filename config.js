7b64dbf072ce4bde2129cadbcf833e6964ca3745
tharuszzz <rushinjhhh@gmail.com>
2025-03-19 19:24:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/WpHbKD3Q/6ec6e0f072121856.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "╭─「 𝚃𝙷𝙰𝚁𝚄𝚂𝙷𝙰-𝙼𝙳 𝙰𝙻𝙸𝚅𝙴 𝙼𝚂𝙶」\𝚗│◈ 𝙿𝚁𝙴𝙵𝙸𝚇 - "."\𝚗│◈ 𝚁𝚄𝙽𝚃𝙸𝙼𝙴 - ${runtime(process.uptime())}\𝚗│◈ 𝙸'𝙼 𝙾𝙽𝙻𝙸𝙽𝙴\𝚗╰──────────●●►",
 };
+
