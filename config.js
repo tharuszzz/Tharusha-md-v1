0b6e1def460f8647279ab87de514d7886e07a207
tharuszzz <rushinjhhh@gmail.com>
2025-03-20 13:10:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "8UgS0QSa#2fZy4DzBxRkK0F1ft5lwCuQ531-YebWXGOjpKIek6uw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/WpHbKD3Q/6ec6e0f072121856.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "╭─「 𝚃𝙷𝙰𝚁𝚄𝚂𝙷𝙰-𝙼𝙳 𝙰𝙻𝙸𝚅𝙴 𝙼𝚂𝙶」\𝚗│◈ 𝙿𝚁𝙴𝙵𝙸𝚇 - "."\𝚗│◈ 𝚁𝚄𝙽𝚃𝙸𝙼𝙴 - ${runtime(process.uptime())}\𝚗│◈ 𝙸'𝙼 𝙾𝙽𝙻𝙸𝙽𝙴\𝚗╰──────────●●►",
+ALIVE_MSG: process.env.ALIVE_MSG || "╭─「 𝚃𝙷𝙰𝚁𝚄𝚂𝙷𝙰-𝙼𝙳 𝙰𝙻𝙸𝚅𝙴 𝙼𝚂𝙶」\𝚗│◈ 𝙿𝚁𝙴𝙵𝙸𝚇 - "."\𝚗│◈ 𝚁𝚄𝙽𝚃𝙸𝙼𝙴 - ${runtime(process.uptime())}\𝚗│◈ 𝙸'𝙼 𝙾𝙽𝙻𝙸𝙽𝙴..👤\𝚗╰──────────●●►",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
 
