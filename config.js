1a271c77f611e3027eeb0c7e428254a498c28bbc
tharuszzz <rushinjhhh@gmail.com>
2025-03-21 19:33:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ const fs = require('fs');
 require('dotenv').config();  // Make sure dotenv is properly required
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "default_session_id",
-    MONGODB: process.env.MONGODB || "mongodb://localhost:27017/mydb",
-};
+    SESSION_ID: process.env.SESSION_ID || "PLQXHCIS#NXYxCs0OaEKuyUS-mivTccn7O0WDJzxxpSX35INFMHQ",
+    MONGODB: process.env.MONGODB || "mongo:BXIkWowtOhTgmCXYZeocahlfwBtSvTmx@nozomi.proxy.rlwy.net:55793"
